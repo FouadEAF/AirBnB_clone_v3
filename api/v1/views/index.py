37a4cbcdd2d436860965e1e3eeedc5b95a70004f
FouadEAF <fouad.azbi@gmail.com>
2024-01-07 06:41:02 -0800

    It’s time to start your API!

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,22 @@
-from flask import jsonify
+#!/usr/bin/python3
+""" Index file """
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """ Returns a JSON response for the status """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ Returns the number of each instance type """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
