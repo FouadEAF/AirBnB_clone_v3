e0bd9946294124af787994728e4c6115f56d213d
FouadEAF <fouad.azbi@gmail.com>
2024-01-07 11:11:38 -0800

    404 error add it

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,40 @@
-#!/usr/bin/venv python3
-from flask import Flask
+#!/usr/bin/python3
+""" Script that imports a Blueprint and runs Flask """
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-import os
+from os import getenv
+from flasgger import Swagger
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.config['SWAGGER'] = {
+    "swagger_version": "2.0",
+    "title": "Flasgger",
+    "headers": [
+        ('Access-Control-Allow-Origin', '*'),
+        ('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS"),
+        ('Access-Control-Allow-Credentials', "true"),
+    ],
+    "specs": [
+        {
+            "version": "1.0",
+            "title": "HBNB API",
+            "endpoint": 'v1_views',
+            "description": 'HBNB REST API',
+            "route": '/v1/views',
+        }
+    ]
+}
+swagger = Swagger(app)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """ Clase the storage """
+def teardown_session(exception):
+    """ Closes storage session """
     storage.close()
 
 
@@ -20,7 +44,10 @@ def not_found(error):
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
-if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
+if __name__ == '__main__':
+    HBNB_API_HOST = getenv('HBNB_API_HOST')
+    HBNB_API_PORT = getenv('HBNB_API_PORT')
+
+    host = '0.0.0.0' if not HBNB_API_HOST else HBNB_API_HOST
+    port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
     app.run(host=host, port=port, threaded=True)
